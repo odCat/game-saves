c625bb2169292410ce0820734f7e603ef8a61f0a
Mihai Gătejescu <gus666xe@gmail.com>
2022-02-03 23:58:36 +0200

    Generate folder name base on timestamp

--- save.py
+++ save.py
@@ -4,6 +4,8 @@ import os
 import json
 import shutil
 
+import datetime
+
 
 def load_game_paths(file_name):
     with open(file_name, 'r') as file:
@@ -28,8 +30,17 @@ def copy_save_file(source, target):
     shutil.copytree(source, target)
 
 
+def generate_folder_name():
+    result = datetime.datetime.now().date().isoformat()
+    result += ' ' + str(datetime.datetime.now().hour)
+    result += ':' + str(datetime.datetime.now().minute)
+    result += ':' + str(datetime.datetime.now().second)
+    return result
+
+
 if __name__ == '__main__':
     pass
 
 # TODO
 # Does shutil.copytree need a try/catch block?
+# Is it needed to use the name to generate the name of the saves folder?
