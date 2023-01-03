eeb2b75440ffe890cd98ab4e22d135d82e088ea4
Mihai Gătejescu <gus666xe@gmail.com>
2023-01-03 16:44:29 +0200

    Update copyright years

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 #!python
 
-#   Copyright 2022 Mihai Gătejescu
+#   Copyright 2022, 2023 Mihai Gătejescu
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
